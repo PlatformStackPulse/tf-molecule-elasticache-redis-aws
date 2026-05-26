432b8c1df9d95e568e8658d7a091d8174fcf6904
PE Stack Pulse <harry.sarshogh@gmail.com>
2026-05-27 00:42:14 +0100

    feat: implement tf-molecule-elasticache-redis-aws molecule

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,34 @@
-output "enabled" {
-  description = "Whether the module is enabled."
-  value       = local.enabled
+output "primary_endpoint" {
+  description = "Primary endpoint address for writes"
+  value       = module.replication_group.primary_endpoint_address
+}
+
+output "reader_endpoint" {
+  description = "Reader endpoint address for reads"
+  value       = module.replication_group.reader_endpoint_address
+}
+
+output "port" {
+  description = "Redis port"
+  value       = module.replication_group.port
+}
+
+output "arn" {
+  description = "ARN of the replication group"
+  value       = module.replication_group.arn
+}
+
+output "id" {
+  description = "ID of the replication group"
+  value       = module.replication_group.id
+}
+
+output "subnet_group_name" {
+  description = "Name of the subnet group"
+  value       = module.subnet_group.name
+}
+
+output "parameter_group_name" {
+  description = "Name of the parameter group"
+  value       = module.parameter_group.name
 }
